b21dd0253c6d3d286b8e0d206085ae0ee9cb1bf8
XCulloden <Culloden.x@etoncollege.org.uk>
2024-01-18 22:47:49 +0000

    Initial Commit

---
+++ CarCustomiserXC/CarCustomiserXCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserXCApp.swift
+//  CarCustomiserXC
+//
+//  Created by xan culloden on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserXCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
